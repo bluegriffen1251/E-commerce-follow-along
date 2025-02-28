bbe5ce2fbbc8a17ae8da64f52c2319cf5ef1ef7d
Jagathpriyan <bluegriffen1856@gmail.com>
2025-02-28 11:28:40 +0530

    23

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,12 @@
+
 import React from 'react'
-<<<<<<< HEAD
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,SignupPage,Home,CreateProduct,MyProducts} from "./Routes";
-import "./App.css";
-import ProductDetails from './pages/productDetails';
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart,ProductDetails,Profile,CreateAddress} from "./Routes";
+import { LoginPage, SignupPage, Home, CreateProduct, MyProducts, Cart,ProductDetails,Profile,CreateAddress,SelectAddress} from "./Routes";
 import "./App.css";
->>>>>>> 532cadd (milestone 22)
 
 const App = () => {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/create-product" element={<CreateProduct />} />
-      <Route path="/my-products" element={<MyProducts/>} />
-      <Route path="/product/:id" element={<ProductDetails />} />
-    </Routes>
-    </BrowserRouter>
-    
-  )
-}
-
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
@@ -39,9 +18,9 @@ const App = () => {
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/profile" element={<Profile />} />
         <Route path='/create-address' element={<CreateAddress />} />
+        <Route path="/select-address" element={<SelectAddress />} />
       </Routes>
     </BrowserRouter>
   );
 };
->>>>>>> 532cadd (milestone 22)
 export default App
